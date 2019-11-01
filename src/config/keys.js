4eae9d1d5c9d05b5f00276fa94ab0242f6b7ed56
anthony gawbill <gaw316@gmail.com>
2019-11-01 00:23:29 -0400

    hello

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,12 +1,4 @@
 import { dev } from "./dev";
 import { prod } from "./prod";
 
-let keys;
-
-if (process.env.NODE_ENV === "production") {
-  keys = prod;
-} else {
-  keys = dev;
-}
-
-export default keys;
+export const keys = process.env.NODE_ENV === "production" ? prod : dev;
