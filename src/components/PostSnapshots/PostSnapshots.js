1e5f6f2453e4e19134b740a4c836da756547b998
anthony gawbill <gaw316@gmail.com>
2019-11-04 19:08:12 -0500

    hello

--- src/components/PostSnapshots/PostSnapshots.js
+++ src/components/PostSnapshots/PostSnapshots.js
@@ -6,23 +6,26 @@ const PostSnapshots = props => {
   const postHandler = (id, slug) => {
     props.history.push({ pathname: `/blog/${slug}`, search: `?id=${id}` });
   };
-  const postsContainer = props.posts.reverse.slice(0, 8).map(post => {
-    return (
-      <PostSnapshot
-        image={post.images[0].url}
-        category={post.category}
-        body={post.body}
-        title={post.title}
-        user={post.user.username}
-        email={post.email}
-        headline={post.headline}
-        dateCreated={post.created_at}
-        clicked={() => postHandler(post.id, post.slug)}
-        id={post.id}
-        key={post.id}
-      />
-    );
-  });
+  const postsContainer = props.posts
+    .reverse()
+    .slice(0, 8)
+    .map(post => {
+      return (
+        <PostSnapshot
+          image={post.images[0].url}
+          category={post.category}
+          body={post.body}
+          title={post.title}
+          user={post.user.username}
+          email={post.email}
+          headline={post.headline}
+          dateCreated={post.created_at}
+          clicked={() => postHandler(post.id, post.slug)}
+          id={post.id}
+          key={post.id}
+        />
+      );
+    });
 
   return <>{postsContainer}</>;
 };
