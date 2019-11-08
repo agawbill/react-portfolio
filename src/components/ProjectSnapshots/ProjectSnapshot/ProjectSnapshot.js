87cb2a4cd6fc3b3fd937c29f3f387e04bc90fe76
anthony gawbill <gaw316@gmail.com>
2019-11-08 02:15:35 -0500

    hello

--- src/components/ProjectSnapshots/ProjectSnapshot/ProjectSnapshot.js
+++ src/components/ProjectSnapshots/ProjectSnapshot/ProjectSnapshot.js
@@ -3,14 +3,16 @@ import { withRouter } from "react-router-dom";
 import styles from "./ProjectSnapshot.module.css";
 
 const ProjectSnapshot = props => {
+  let wrapperClass = null;
   let classes = [styles.ProjectSnapshot, styles.WidthLanding];
   if (props.location !== undefined) {
     if (props.location.pathname === "/projects") {
+      wrapperClass = styles.Wrapper;
       classes = [styles.ProjectSnapshot, styles.WidthProjects];
     }
   }
   return (
-    <div className={styles.Wrapper}>
+    <div className={wrapperClass}>
       <div className={classes.join(" ")} onClick={props.clicked}>
         <div className={styles.ProjectSnapshotIndicator} />
         <div
