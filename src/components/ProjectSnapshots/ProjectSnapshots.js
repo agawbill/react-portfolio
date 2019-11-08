d07ea53c3337933d0fb2e99a1fa66e1da244e8e9
anthony gawbill <gaw316@gmail.com>
2019-11-08 02:09:15 -0500

    hello

--- src/components/ProjectSnapshots/ProjectSnapshots.js
+++ src/components/ProjectSnapshots/ProjectSnapshots.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import styles from "./ProjectSnapshots.module.css";
 import ProjectSnapshot from "./ProjectSnapshot/ProjectSnapshot";
 
 const ProjectSnapshots = props => {
