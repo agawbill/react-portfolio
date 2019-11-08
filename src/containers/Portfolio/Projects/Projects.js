ec5ca260f932feed534c616684d4ad3f6beddf66
anthony gawbill <gaw316@gmail.com>
2019-11-08 18:38:13 -0500

    hello

--- src/containers/Portfolio/Projects/Projects.js
+++ src/containers/Portfolio/Projects/Projects.js
@@ -5,12 +5,20 @@ import Header from "../../../components/UI/Header/Header";
 import styles from "./Projects.module.css";
 
 const Projects = props => {
-  const projects = useSelector(state => state.projects.projects);
+  const sectionDescription = useSelector(state => state.content.projects);
+  console.log(sectionDescription);
+
+  const selectedProjects = useSelector(
+    state => state.projects.selectedProjects
+  );
+  const remainingProjects = useSelector(
+    state => state.projects.remainingProjects
+  );
   return (
     <>
       <Header type="projects" title=" Projects" />
       <div className={styles.Projects}>
-        <ProjectSnapshots projects={projects} />
+        <ProjectSnapshots projects={selectedProjects} />
       </div>
     </>
   );
